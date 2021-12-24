a85b2af39b20bbdb140d0455108d4de520f214c4
Pankaj Sharma <sharmapankaj2512@gmail.com>
2021-12-24 20:52:40 +0100

    :recycle: REFACTOR make amount private

--- Money.Tests/Dollar.cs
+++ Money.Tests/Dollar.cs
@@ -2,30 +2,30 @@ namespace Money.Tests;
 
 public class Dollar
 {
-    public int amount;
+    private readonly int _amount;
 
     protected bool Equals(Dollar other)
     {
-        return amount == other.amount;
+        return _amount == other._amount;
     }
 
     public override bool Equals(object? obj)
     {
-        return amount == (obj as Dollar).amount;
+        return _amount == (obj as Dollar)._amount;
     }
 
     public override int GetHashCode()
     {
-        return amount;
+        return _amount;
     }
 
     public Dollar(int amount)
     {
-        this.amount = amount;
+        this._amount = amount;
     }
 
     public Dollar Times(int multiplier)
     {
-        return new Dollar(amount * multiplier);
+        return new Dollar(_amount * multiplier);
     }
 }
