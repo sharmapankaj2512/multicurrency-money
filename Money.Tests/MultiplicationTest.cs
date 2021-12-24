8751d1e70bc8e55098f6333ce0c29d8b331a0bd5
Pankaj Sharma <sharmapankaj2512@gmail.com>
2021-12-24 22:54:12 +0100

    :recycle: REFACTOR rename test

--- Money.Tests/MultiplicationTest.cs
+++ Money.Tests/MultiplicationTest.cs
@@ -6,7 +6,7 @@ namespace Money.Tests;
 public class MultiplicationTest
 {
     [Test]
-    public void TestDollarMultiplication()
+    public void TestMultiplication()
     {
         AreEqual(Money.Dollar(10), Money.Dollar(5).Times(2));
         AreEqual(Money.Dollar(15), Money.Dollar(5).Times(3));
