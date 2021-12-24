ca0fef108c4ccedf116d82fc0305c8ec4c57178c
Pankaj Sharma <sharmapankaj2512@gmail.com>
2021-12-24 22:53:49 +0100

    :recycle: REFACTOR merge multiplication tests as the behavior is same in both cases

--- Money.Tests/MultiplicationTest.cs
+++ Money.Tests/MultiplicationTest.cs
@@ -8,16 +8,9 @@ public class MultiplicationTest
     [Test]
     public void TestDollarMultiplication()
     {
-        var five = Money.Dollar(5);
-        AreEqual(Money.Dollar(10), five.Times(2));
-        AreEqual(Money.Dollar(15), five.Times(3));
-    }
-    
-    [Test]
-    public void TestFrancMultiplication()
-    {
-        var five = Money.Franc(5);
-        AreEqual(Money.Franc(10), five.Times(2));
-        AreEqual(Money.Franc(15), five.Times(3));
+        AreEqual(Money.Dollar(10), Money.Dollar(5).Times(2));
+        AreEqual(Money.Dollar(15), Money.Dollar(5).Times(3));
+        AreEqual(Money.Franc(10), Money.Franc(5).Times(2));
+        AreEqual(Money.Franc(15), Money.Franc(5).Times(3));
     }
 }
