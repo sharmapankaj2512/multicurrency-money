e34244dcb4eb9cb38c19977635f9aae2bd112a00
Pankaj Sharma <sharmapankaj2512@gmail.com>
2021-12-25 18:31:25 +0100

    :recycle: REFACTOR use bank rates to fetch the rate

--- Money.Tests/Bank.cs
+++ Money.Tests/Bank.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Collections.Generic;
+
 namespace Money.Tests;
 
 public class Bank
 {
+    private Dictionary<Tuple<string, string>, int> _rates = new();
+
     public Money Reduce(IExpression expr, string to)
     {
         return expr.Reduce(this, to);
@@ -9,11 +14,11 @@ public class Bank
 
     public void AddRate(string from, string to, int rate)
     {
-        
+        _rates[Tuple.Create(from, to)] = rate;
     }
 
     public int Rate(string from, string to)
     {
-        return from == "CHF" && to == "USD"? 2 : 1;
+        return from == to ? 1 : _rates[Tuple.Create(from, to)];
     }
 }
